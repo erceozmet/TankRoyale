09722d3bfaf69fcc1d62f2175a360d0e5e72872f
Erce Ozmetin <55060224+erceozmet@users.noreply.github.com>
2022-03-28 13:21:38 -0400

    revorked

---
+++ src/arena.config.ts
@@ -0,0 +1,41 @@
+import Arena from "@colyseus/arena";
+import { monitor } from "@colyseus/monitor";
+
+/**
+ * Import your Room files
+ */
+import { MyRoom } from "./rooms/MyRoom";
+
+export default Arena({
+    getId: () => "Tank Royale",
+
+    initializeGameServer: (gameServer) => {
+        /**
+         * Define your room handlers:
+         */
+        gameServer.define('my_room', MyRoom);
+    },
+
+    initializeExpress: (app) => {
+        /**
+         * Bind your custom express routes here:
+         */
+        app.get("/", (req, res) => {
+            res.send("Connected to backend server");
+        });
+
+        /**
+         * Bind @colyseus/monitor
+         * It is recommended to protect this route with a password.
+         * Read more: https://docs.colyseus.io/tools/monitor/
+         */
+        app.use("/colyseus", monitor());
+    },
+
+
+    beforeListen: () => {
+        /**
+         * Before before gameServer.listen() is called.
+         */
+    }
+});
