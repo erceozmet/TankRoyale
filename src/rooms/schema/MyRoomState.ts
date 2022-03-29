ab2f523ae1533be62ce366a31e750f5a9b5aa247
Erce Ozmetin <55060224+erceozmet@users.noreply.github.com>
2022-03-28 21:55:54 -0400

    Update MyRoomState.ts

--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -1,10 +1,19 @@
 import { Schema, ArraySchema, MapSchema, Context, type } from "@colyseus/schema";
 
 export class MyRoomState extends Schema {
+ 
 
+  map: Array<number>;
+  
+
+<<<<<<< Updated upstream
   player_size:  number = 20;
   // @type(["number"]) map = new ArraySchema<number>();
   @type(["number"]) mynum: number = 0;
   @type(["string"]) client_addresses = new ArraySchema<string>();
+=======
+  @type("string") mySynchronizedProperty: string = "Hello world";
+  
+>>>>>>> Stashed changes
 
 }
