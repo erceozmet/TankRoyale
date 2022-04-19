a7891321248aa79a7433c2240db856d9420fd602
dcolak01 <54953926+dcolak01@users.noreply.github.com>
2022-04-19 13:07:22 -0400

    Update MyRoomState.ts

--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -4,5 +4,5 @@ import { GameMap } from "./GameMap";
 export class MyRoomState extends Schema {
     @type(GameMap) map: GameMap = new GameMap();
     player_count: number = 0;
-    player_size: number = 16;
+    player_size: number = 2;
 }
