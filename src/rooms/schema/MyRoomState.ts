b21092d9eedde5fd018583b10169ccf2f749b5bd
dcolak01 <54953926+dcolak01@users.noreply.github.com>
2022-04-20 01:54:29 -0400

    waiting room change

--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -3,6 +3,5 @@ import { GameMap } from "./GameMap";
 
 export class MyRoomState extends Schema {
     @type(GameMap) map: GameMap = new GameMap();
-    player_count: number = 0;
     player_size: number = 2;
 }
